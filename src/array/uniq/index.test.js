c73397ef3da90e1686b75fa912e54d821e9ca443
Mazurenko <vmazurenko913@gmail.com>
2020-05-14 17:47:50 +0300

    fix uniq function

--- src/array/uniq/index.test.js
+++ src/array/uniq/index.test.js
@@ -1,18 +1,9 @@
 const { uniq } = require('./index')
 
-it('should return new array of number were only the first occurrence of each element is kept', () => {
-  const testArray = [1, 2, 3, 2, 1]
-  expect(uniq(testArray)).toEqual([1, 2, 3])
-})
-
-it('should return new array of string were only the first occurrence of each element is kept', () => {
-  const testArray = ['a', 'b', 'c', 'a', 'b']
-  expect(uniq(testArray)).toEqual(['a', 'b', 'c'])
-})
-
-it('should return new array of mixed value were only the first occurrence of each element is kept', () => {
-  const testArray = ['a', 1, 'b', 2, 'a', 1, 'b', 2]
-  expect(uniq(testArray)).toEqual(['a', 1, 'b', 2])
+it('should return new array were only the first occurrence of each element is kept', () => {
+  expect(uniq([1, 2, 3, 2, 1])).toEqual([1, 2, 3])
+  expect(uniq(['a', 'b', 'c', 'a', 'b'])).toEqual(['a', 'b', 'c'])
+  expect(uniq(['a', 1, 'b', 2, 'a', 1, 'b', 2])).toEqual(['a', 1, 'b', 2])
 })
 
 it('should return undefined if argument is not array', () => {
