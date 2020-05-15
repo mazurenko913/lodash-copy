cdf0893d9bf63fbe56c508c6f9549b788301a5a4
Mazurenko <vmazurenko913@gmail.com>
2020-05-15 14:08:11 +0300

    fix union

--- src/array/union/index.test.js
+++ src/array/union/index.test.js
@@ -6,11 +6,11 @@ it('should creates array were only unique values is kept, from all given arrays'
   expect(union([['a', 1, 'b'], ['b', 2, 3, undefined], ['c', 2]])).toEqual(['a', 1, 'b', 2, 3, undefined, 'c'])
 })
 
-// it('should  not delate any items if input array is uniq', () => {
-//   expect(union([[1, 2], [3, 4], [5, 6]])).toEqual([1, 2, 3, 4, 5, 6])
-//   expect(union([['a', 'b'], ['c', 'd'], ['e', 'f']])).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
-//   expect(union([['a', 1], ['b', 2], ['c', 3]])).toEqual(['a', 1, 'b', 2, 'c', 3])
-// })
+it('should  not delate any items if input array is uniq', () => {
+  expect(union([[1, 2], [3, 4], [5, 6]])).toEqual([1, 2, 3, 4, 5, 6])
+  expect(union([['a', 'b'], ['c', 'd'], ['e', 'f']])).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
+  expect(union([['a', 1], ['b', 2], ['c', 3]])).toEqual(['a', 1, 'b', 2, 'c', 3])
+})
 
 it('should return undefined if argument is not array', () => {
   expect(union(123)).toBe(undefined)
