c412ebab7e36bea1fca08576e40b98b9f6e015e3
Mazurenko <vmazurenko913@gmail.com>
2020-05-18 15:21:48 +0300

    fix

--- src/array/indexOf/index.js
+++ src/array/indexOf/index.js
@@ -2,10 +2,18 @@ function indexOf (array, value, fromIndex = 0) {
   if (!Array.isArray(array)) {
     return undefined
   }
+  if (fromIndex >= array.length) {
+    return -1
+  }
+  if (fromIndex < 0) {
+    return -1
+  }
 
-  for (let i = 0 + fromIndex; i < array.length; i++) {
+  for (let i = fromIndex; i < array.length; i++) {
     if (array[i] === value) {
       return i
+    } else if (array[i] !== value) {
+      return -1
     }
   }
 }
--- src/array/indexOf/index.test.js
+++ src/array/indexOf/index.test.js
@@ -6,6 +6,12 @@ it('should return the index of number at which the first occurrence of value is 
   expect(indexOf(['a', 2, 'c', 4, 'd', undefined], 4)).toEqual(3)
 })
 
+it('should return  -1 when fromIndex is negative, the element is not in the array or bigger than array', () => {
+  expect(indexOf([1, 2, 3], 5)).toEqual(-1)
+  expect(indexOf([1, 2, 3], -2)).toEqual(-1)
+  expect(indexOf([1, 2, 3], 1, 5)).toEqual(-1)
+})
+
 it('should return undefined if argument is not array', () => {
   expect(indexOf(123)).toBe(undefined)
   expect(indexOf('string')).toBe(undefined)
