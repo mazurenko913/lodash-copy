7f8f8424814d8e5e35afbef72c474a73101f8f91
Mazurenko <vmazurenko913@gmail.com>
2020-05-21 20:23:19 +0300

    make fromParis function

--- src/array/fromPairs/index.js
+++ src/array/fromPairs/index.js
@@ -1,8 +1,4 @@
 function fromParis (array) {
-  // if (!Array.isArray(array)) {
-  //   return undefined
-  // }
-
   return array.reduce((acc, el) => ({ ...acc, [el[0]]: el[1] }), {})
 }
 module.exports = { fromParis }
--- src/array/fromPairs/index.test.js
+++ src/array/fromPairs/index.test.js
@@ -4,7 +4,18 @@ it('should return an object composed from key-value pairs', () => {
   expect(fromParis([['a', 1], ['b', 2]])).toStrictEqual({ a: 1, b: 2 })
 })
 
-// it('should return undefined if argument is not array', () => {
-//   expect(fromParis(123)).toBe(undefined)
-//   expect(fromParis('string')).toBe(undefined)
-// })
+it('should return an object were kay is not a string', () => {
+  expect(fromParis([[2, 1], [undefined, 2], [NaN, 3]])).toStrictEqual({ 2: 1, undefined: 2, NaN: 3 })
+})
+
+it('should return an object were kay is object or array', () => {
+  expect(fromParis([[[], 7], [{}, 'a'], [[2], 4]])).toStrictEqual({ '': 7, '[object Object]': 'a', 2: 4 })
+})
+
+it('should return an object were values more then 2', () => {
+  expect(fromParis([['a', 5, 4], ['b', 6, 3]])).toStrictEqual({ a: 5, b: 6 })
+})
+
+it('should return an object were values less then 2', () => {
+  expect(fromParis([['a'], ['b']])).toStrictEqual({ a: undefined, b: undefined })
+})
