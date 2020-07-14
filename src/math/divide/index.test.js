c64c697cc0818d1816cc496d0ed9a26c0f4018d5
Mazurenko <vmazurenko913@gmail.com>
2020-07-14 20:03:43 +0300

    fix divide function

--- src/math/divide/index.test.js
+++ src/math/divide/index.test.js
@@ -6,12 +6,12 @@ it('should divide two numbers.', () => {
   expect(divide(4, [2])).toEqual(2)
 })
 
-it('if numbers more then 2, should ignore it.', () => {
+it('should ignore numbers? if they more then 2.', () => {
   expect(divide(1, 2, 4)).toEqual(0.5)
   expect(divide(4, -2, 1)).toEqual(-2)
 })
 
-it('if one of argument is not a number should return NaN', () => {
+it('should return NaN, if one of argument is not a number.', () => {
   expect(divide(4, 'a')).toEqual(NaN)
   expect(divide(4, NaN)).toEqual(NaN)
   expect(divide('a', 'a')).toEqual(NaN)
