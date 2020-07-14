5c0753a9bbb43cecb96f44c0b866977b65f56202
Mazurenko <vmazurenko913@gmail.com>
2020-07-14 20:07:10 +0300

    fix add function

--- src/math/add/index.test.js
+++ src/math/add/index.test.js
@@ -5,7 +5,7 @@ it('should adds two numbers.', () => {
   expect(add(0, -2)).toEqual(-2)
 })
 
-it('if one of argument is not a number should add it too', () => {
+it('should concat value if one of argument is string', () => {
   expect(add(1, 'a')).toEqual('1a')
   expect(add('a', NaN)).toEqual('aNaN')
   expect(add('a', [1])).toEqual('a1')
@@ -13,7 +13,7 @@ it('if one of argument is not a number should add it too', () => {
   expect(add('a', { a: 1 })).toEqual('a[object Object]')
 })
 
-it('if numbers more then 2, should ignore it. ', () => {
+it('should ignore numbers, if they more then 2.', () => {
   expect(add(1, 3, 4)).toEqual(4)
   expect(add(0, -2, 1)).toEqual(-2)
 })
