4068147f6fb24641738056694b37fb576aa15f9d
Mazurenko <vmazurenko913@gmail.com>
2020-07-14 19:51:29 +0300

    fix inRange function

--- src/number/inRange/index.test.js
+++ src/number/inRange/index.test.js
@@ -1,26 +1,26 @@
 const { inRange } = require('./index')
 
-it('should check if n is between start and up to, but not including, end', () => {
+it('should check if n is between start and up to.', () => {
   expect(inRange(2, 0, 3)).toEqual(true)
   expect(inRange(2, [0], 3)).toEqual(true)
   expect(inRange(7, 0, 6)).toEqual(false)
 })
 
-it('If start is greater than end the params are swapped to support negative ranges.', () => {
+it('should swap params if start is greater than end.', () => {
   expect(inRange(2, 4, 1)).toEqual(true)
   expect(inRange(7, 9, 6)).toEqual(true)
 })
 
-it('If end is not specified, it is set to start with start then set to 0.', () => {
+it('should the end set to start if the end not specified.', () => {
   expect(inRange(2, 5)).toEqual(false)
   expect(inRange(7, 9)).toEqual(false)
 })
 
-it('If arguments more then 3 ignore it', () => {
+it('should ignore arguments if they more then 3', () => {
   expect(inRange(2, 1, 6, 3)).toEqual(true)
 })
 
-it('If one of argument is not a number, should return false', () => {
+it('should return false, if one of argument is not a number,', () => {
   expect(inRange('a', 2, 5)).toEqual(false)
   expect(inRange(2, NaN, 5)).toEqual(false)
   expect(inRange('a', 2, undefined)).toEqual(false)
