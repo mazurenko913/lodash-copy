0ec0f88b339ea8d7b4818856da06a896a5158ba8
Mazurenko <vmazurenko913@gmail.com>
2020-07-16 18:10:14 +0300

    fix toLower function

--- src/string/toLower/index.js
+++ src/string/toLower/index.js
@@ -1,10 +1,5 @@
-function toLower (string) {
-  if (typeof (string) !== 'string') {
-    const lower = '' + string
-    return lower.toLowerCase()
-  }
-
-  return string.toLowerCase()
+function toLower (value) {
+  return String(value).toLowerCase()
 }
 
 module.exports = { toLower }
