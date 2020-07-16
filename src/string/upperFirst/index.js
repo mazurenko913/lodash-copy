09f95efb283e6113b59ff3ffafb82772ae6f1b60
Mazurenko <vmazurenko913@gmail.com>
2020-07-16 18:37:28 +0300

    fix upperFirst function

--- src/string/upperFirst/index.js
+++ src/string/upperFirst/index.js
@@ -1,7 +1,7 @@
 function upperFirst (string) {
   if (typeof (string) !== 'string') {
-    const upper = '' + string
-    return upper[0].toUpperCase() + upper.slice(1)
+    string = '' + string
+    string = string[0].toUpperCase() + string.slice(1)
   }
 
   return string && string[0].toUpperCase() + string.slice(1)
