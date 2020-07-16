1a087d1dc55c24be123bdcdd6da044005b5c30df
Mazurenko <vmazurenko913@gmail.com>
2020-07-16 18:14:09 +0300

    fix replace function

--- src/string/replace/index.js
+++ src/string/replace/index.js
@@ -1,18 +1,5 @@
 function replace (string, pattern, replacement) {
-  if (typeof (string) !== 'string') {
-    const str = '' + string
-    return str.replace(pattern, replacement)
-  }
-  if (typeof (replacement) !== 'string') {
-    const str = '' + replacement
-    return string.replace(pattern, str)
-  }
-
-  if (string === '') {
-    return ''
-  }
-
-  return string.replace(pattern, replacement)
+  return String(string).replace(pattern, String(replacement))
 }
 
 module.exports = { replace }
