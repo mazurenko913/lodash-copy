f040eb7a05000a3c28129c543fb2ee5be61572b9
Mazurenko <vmazurenko913@gmail.com>
2020-07-16 20:33:13 +0300

    fix unescape function

--- src/string/unescape/index.js
+++ src/string/unescape/index.js
@@ -1,8 +1,20 @@
+const unescapeCharacters = [
+  ['&amp;', '&'],
+  ['&lt;', '<'],
+  ['&gt;', '>'],
+  ['&quot;', '"']
+]
+
 function unescape (string) {
   if (typeof (string) !== 'string') {
-    return '' + string
+    string = '' + string
   }
-  return string && string.replace('&amp;', '&').replace('&lt;', '<').replace('&quot;', '"').replace('&gt;', '>')
+
+  unescapeCharacters.forEach((arr) => {
+    string = string.replace(arr[0], arr[1])
+  })
+
+  return string
 }
 
 module.exports = { unescape }
