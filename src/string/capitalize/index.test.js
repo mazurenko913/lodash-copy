9e2533f5dba9c0f1dbb802a5955a85f20fa45576
Mazurenko <vmazurenko913@gmail.com>
2020-07-17 10:59:48 +0300

    fix capitalize function

--- src/string/capitalize/index.test.js
+++ src/string/capitalize/index.test.js
@@ -4,15 +4,11 @@ it('should Convert the first character of string to upper case and the remaining
   expect(capitalize('aDOlf')).toEqual('Adolf')
 })
 
-it('should return empty string, if string is empty', () => {
-  expect(capitalize('')).toEqual('')
-})
-
 it('should ignore value if they more then 1', () => {
   expect(capitalize('aff', 'Att')).toEqual('Aff')
 })
 
 it('should convert value to string, if it is not a string', () => {
-  expect(capitalize(NaN)).toEqual('NaN')
-  expect(capitalize(true)).toEqual('true')
+  expect(capitalize(NaN)).toEqual('Nan')
+  expect(capitalize(true)).toEqual('True')
 })
