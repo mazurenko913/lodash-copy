f46f023584fca7b8bf6c91844431c94431048525
Mazurenko <vmazurenko913@gmail.com>
2020-07-20 15:21:28 +0300

    make forEach function

---
+++ src/collection/forEach/index.js
@@ -0,0 +1,18 @@
+const { isObject } = require('../../lang/isObject/index.js')
+const { isArray } = require('../../lang/isArray/index.js')
+
+function forEach (collection, cb) {
+  if (isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      cb(collection[i])
+    }
+  }
+
+  if (isObject(collection) && !Array.isArray(collection)) {
+    for (const key in collection) {
+      cb(key, collection[key])
+    }
+  }
+}
+
+module.exports = { forEach }
