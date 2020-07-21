a90d991372312e0c6d463ac55d199d955e8373bb
Mazurenko <vmazurenko913@gmail.com>
2020-07-21 14:21:21 +0300

    make inclides function

--- src/collection/includes/index.js
+++ src/collection/includes/index.js
@@ -1,8 +1,42 @@
-function includes (value) {
-  if (!Array.isArray(value)) {
-    return [value]
+const { isArray } = require('../../lang/isArray/index.js')
+const { isString } = require('../../lang/isString/index.js')
+const { isObject } = require('../../lang/isObject/index.js')
+
+function includes (collection, value, fromIndex = 0) {
+  if (isArray(collection)) {
+    if (fromIndex < 0) {
+      fromIndex = collection.length + fromIndex
+    }
+
+    for (let i = fromIndex; i < collection.length; i++) {
+      if (collection[i] === value) {
+        return true
+      }
+    }
+    return false
   }
-  return value
+
+  if (isString(collection)) {
+    if ((collection.indexOf(value) >= 0)) {
+      return true
+    }
+    return false
+  }
+
+  if (isObject(collection)) {
+    const keys = Object.values(collection)
+
+    if (fromIndex < 0) {
+      fromIndex = keys.length + fromIndex
+    }
+
+    for (let i = fromIndex; i < keys.length; i++) {
+      if (keys[i] === value) {
+        return true
+      }
+    }
+    return false
+  }
 }
 
 module.exports = { includes }
--- src/collection/includes/index.test.js
+++ src/collection/includes/index.test.js
@@ -1,5 +1,40 @@
 const { includes } = require('./index')
 
-it('should checks if value is in collection.', () => {
-  expect(includes(1, 2, 3], 1)).toEqual([2])
+it('should checks if value is in array.', () => {
+  expect(includes([1, 2, 3], 1)).toEqual(true)
+  expect(includes([1, 2, 3], 4)).toEqual(false)
+})
+
+it('should check an value start fromIndex', () => {
+  expect(includes([1, 2, 3, 2, 1, 4, 2], 2, 2)).toEqual(true)
+})
+
+it('should check an value from the end, if fromIndex is negative', () => {
+  expect(includes([1, 2, 3, 2, 1, 3], 2, -3)).toEqual(true)
+  expect(includes([1, 2, 3, 2, 1, 3], 2, -1)).toEqual(false)
+})
+
+it('should check for a substring of value, if collection is string.', () => {
+  expect(includes('abcd', 'bc')).toEqual(true)
+  expect(includes('abcd', 'qw')).toEqual(false)
+})
+
+it('should work only with 2 arguments, collection and value, other ignore', () => {
+  expect(includes('abcd', 'bc', 1)).toEqual(true)
+  expect(includes('abcd', 'b', 'a')).toEqual(true)
+})
+
+it('should checks if value is in object kays.', () => {
+  expect(includes({ a: 1, b: 2 }, 1)).toEqual(true)
+  expect(includes({ a: 1, b: 2 }, 3)).toEqual(false)
+})
+
+it('should check an value in object start fromIndex', () => {
+  expect(includes({ a: 1, b: 2, c: 3, d: 1 }, 1, 1)).toEqual(true)
+  expect(includes({ a: 1, b: 2, c: 3, d: 1 }, 2, 3)).toEqual(false)
+})
+
+it('should check an value in object from the end, if fromIndex is negative', () => {
+  expect(includes({ a: 1, b: 2, c: 3, d: 1 }, 2, -3)).toEqual(true)
+  expect(includes({ a: 1, b: 2, c: 3, d: 1 }, 2, -1)).toEqual(false)
 })
