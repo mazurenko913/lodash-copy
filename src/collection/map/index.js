a8cf534627b3996d5588b8dc5b4c022ced1e0b3b
Mazurenko <vmazurenko913@gmail.com>
2020-07-22 16:48:17 +0300

    make map function

---
+++ src/collection/map/index.js
@@ -0,0 +1,24 @@
+const { isArray } = require('../../lang/isArray/index.js')
+const { isObject } = require('../../lang/isObject/index.js')
+
+function map (collection, cb) {
+  if (isArray(collection)) {
+    const newArr = []
+    let index = 0
+    for (let i = 0; i < collection.length; i++) {
+      index = collection.indexOf(collection[i])
+      newArr.push(cb(collection[i], index))
+    }
+    return newArr
+  }
+
+  if (isObject(collection, cb)) {
+    const newArr = []
+    Object.keys(collection).forEach(key => {
+      newArr.push(cb(collection[key], key, collection))
+    })
+    return newArr
+  }
+}
+
+module.exports = { map }
---
+++ src/collection/map/index.test.js
@@ -0,0 +1,20 @@
+const { map } = require('./index')
+
+it('should creates a function that square all elements in array.', () => {
+  const arr = [2, 3]
+  const expectArr = [4, 9]
+  const resultArr = map(arr, (value, index) => {
+    return value * value
+  })
+  expect(resultArr).toEqual(expectArr)
+})
+
+it('should creates a function that square all value in object', () => {
+  const obj = { a: 2, b: 3, c: 4 }
+  const expectObj = ['2a', '3b', '4c']
+
+  const resultObj = map(obj, (value, key, collection) => {
+    return value + key
+  })
+  expect(resultObj).toEqual(expectObj)
+})
