460954b7efd6161141123a210f39e6ad5f2d923a
Mazurenko <vmazurenko913@gmail.com>
2020-07-24 12:44:33 +0300

    fix map map

--- src/collection/map/index.test.js
+++ src/collection/map/index.test.js
@@ -1,6 +1,6 @@
 const { map } = require('./index')
 
-it('should creates a function that square all elements in array.', () => {
+it('should creates an array of values by running each element in array thru iteratee.', () => {
   const arr = [2, 3]
   const expectArr = [4, 9]
   const resultArr = map(arr, (value, index, collection) => {
@@ -9,7 +9,7 @@ it('should creates a function that square all elements in array.', () => {
   expect(resultArr).toEqual(expectArr)
 })
 
-it('should creates a function that square all value in object', () => {
+it('should creates an array of values by running each values in object thru iteratee.', () => {
   const obj = { a: 2, b: 3, c: 4 }
   const expectObj = ['2a', '3b', '4c']
 
